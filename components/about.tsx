ba2ee059ad2476eb8b2b4a46d0b659ab71cacfce
Lebohang Precious Ziane <lebohangziane@gmail.com>
2025-10-02 13:11:11 +0200

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -4,21 +4,7 @@ export function About() {
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background/75 px-6 py-5 backdrop-blur lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="text-sm font-bold uppercase tracking-widest text-foreground">About</h2>
       </div>
-      <div className="space-y-4 text-pretty leading-relaxed text-muted-foreground">
-        <p>
-          💻 I am a detail-oriented Software Developer candidate with a Diploma in Information & Communication Technology – Application Development (2024) from Sol Plaatje University, 
-          where I graduated with multiple distinctions in key modules such as Applications Development, Business Practice, 
-          Information Systems, Multimedia Applications, and Project Management.
-        </p>
-        </div> 
-      
-      <div className="space-y-4 text-pretty leading-relaxed text-muted-foreground">
-        <p>
-          ☕ I am an Oracle Certified Associate (Java SE 8 Programmer, IZO-808) and hold certifications in IBM AI Fundamentals, Professional Skill, and 
-          Certiport IT Specialist, which reflect my dedication to continuous learning and staying aligned with industry standards.
-        </p>
-        
-      
+    
       <div className="space-y-4 text-pretty leading-relaxed text-muted-foreground">
         <p>
           I'm a developer passionate about crafting accessible, pixel-perfect user interfaces that blend thoughtful design with robust engineering. 
